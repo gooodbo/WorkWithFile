840a60804122d8464fc9a280c55ad70a162b2d07
Ivan <kykygrom.com@gmail.com>
2019-11-04 03:53:36 +0300

    First Commit

---
+++ src/com/Main.java
@@ -0,0 +1,111 @@
+package com;
+
+
+import java.io.*;
+import java.net.URI;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.HashMap;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String s;
+        String textFile = "";
+        String URL = "https://drive.google.com/uc?authuser=0&id=1y2rnReNIUzpWzP1RWJLGbUY0CCzSRkqa&export=download";
+        String path = "C:\\Users\\Avalakh\\Desktop\\voina-i-mir.txt";
+
+        try {
+            download(URL, path);
+            BufferedReader bufferedReader = new BufferedReader(
+                    new InputStreamReader(
+                            new FileInputStream(
+                                    "C:\\Users\\Avalakh\\Desktop\\voina-i-mir.txt"), StandardCharsets.UTF_8));
+            while ((s = bufferedReader.readLine()) != null) {
+                textFile = textFile + s + "\n";
+            }
+            bufferedReader.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        String[] textFileArray = textFile.split("\\s*([.?,!])\\s*|\\s");
+
+        //System.out.println(textFile);
+
+        System.out.println("Самое повторяющееся слово: \"" + theMostRepeatedWord(textFileArray) + "\".");
+        System.out.println("Самое редко встречающееся: \"" + theRarestWord(textFileArray) + "\" .");
+        System.out.println("Самое длинное слово \"" + theLongestWord(textFileArray) + "\".");
+
+
+    }
+
+
+    public static String theLongestWord(String[] m) {
+
+        int i, size = m.length;
+        int iMax = -1, iMaxLength = -1;
+        for (i = 0; i < size; ++i)
+            if (!"".equals(m[i]) && m[i].length() > iMaxLength) {
+                iMax = i;
+                iMaxLength = m[i].length();
+            }
+
+        return m[iMax];
+    }
+
+
+    public static String theRarestWord(String[] m) {
+        HashMap<String, Integer> h = new HashMap<>();
+
+        for (int i = 0; i < m.length; i++) {
+            if (h.containsKey(m[i])) {
+                h.replace(m[i], h.get(m[i]) + 1);
+            } else {
+                h.put(m[i], 1);
+            }
+        }
+        String wordRarest = "";
+
+        int p1 = 0;
+        for (String w : h.keySet()) {
+            wordRarest = w;
+            if (p1 > h.get(w)) {
+                p1 = h.get(w);
+                wordRarest = w;
+            }
+        }
+        return wordRarest;
+    }
+
+
+    public static String theMostRepeatedWord(String[] m) {
+        HashMap<String, Integer> h = new HashMap<>();
+
+        for (int i = 0; i < m.length; i++) {
+            if (h.containsKey(m[i])) {
+                h.replace(m[i], h.get(m[i]) + 1);
+            } else {
+                h.put(m[i], 1);
+            }
+        }
+
+        String wordMostRepeated = null;
+        int p = 0;
+        for (String w : h.keySet()) {
+            if (p < h.get(w)) {
+                p = h.get(w);
+                wordMostRepeated = w;
+            }
+        }
+        return wordMostRepeated;
+    }
+
+
+    public static void download(String url, String fileName) throws Exception {
+        try (InputStream in = URI.create(url).toURL().openStream()) {
+            Files.copy(in, Paths.get(fileName));
+        }
+    }
+}
+
